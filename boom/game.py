4f9c81adcb02e8d53e6928fb6550f0ad70694b7b
Matt Haggard <haggardii@gmail.com>
2012-06-04 22:09:25 -0600

    Added ignite to Bomb

--- boom/game.py
+++ boom/game.py
@@ -1,5 +1,13 @@
+from twisted.internet import reactor
+from twisted.internet.defer import Deferred
+
 class Bomb:
     
     def __init__(self, fuse, size):
         self.fuse = fuse
         self.size = size
+
+    def ignite(self):
+        d = Deferred()
+        reactor.callLater(self.fuse, d.callback, self)
+        return d
--- boom/test/test_game.py
+++ boom/test/test_game.py
@@ -1,10 +1,19 @@
-from unittest import TestCase
+from twisted.trial.unittest import TestCase
 
 from boom.game import Bomb
 
 class BombTest(TestCase):
 
+    timeout = 2
+
     def test_init(self):
         bomb = Bomb(3, 4)
         self.assertEqual(bomb.fuse, 3)
         self.assertEqual(bomb.size, 4)
+
+    def test_ignite(self):
+        bomb = Bomb(1, 4)
+        d = bomb.ignite()
+        def check(result):
+            self.assertEqual(result, bomb)
+        return d.addCallback(check)
