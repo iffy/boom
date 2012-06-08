08026058a888f7bf9521a85762c87380c616dc20
Matt Haggard <haggardii@gmail.com>
2012-06-07 22:38:36 -0600

    Bombs can be dropped on generated boards

--- boom/game.py
+++ boom/game.py
@@ -1,6 +1,63 @@
 from twisted.internet import reactor
 from twisted.internet.defer import Deferred
 
+EMPTY = 0
+HARD = 1
+SOFT = 2
+
+
+class Board:
+    """
+    The board is made of 3 layers:
+        
+        1. Background: Tiles are all the same currently.  In the
+           future, if things like conveyor belts or wormholes are
+           to be supported, they will appear in this layer.
+
+        2. Foreground: Tiles are either EMPTY, HARD or SOFT
+        
+        3. Players/bombs/items: Tiles may have one or more of the
+           moveable things on it (players, bombs, powerups).
+
+    XXX vars
+    """
+
+
+    def __init__(self, reactor=reactor):
+        self.fg_tiles = {}
+        self._reactor = reactor
+
+
+    def generate(self, width, height):
+        """
+        XXX
+        """
+        for x in xrange(width):
+            for y in xrange(height):
+                coord = (x,y)
+                if (x % 2) and (y % 2):
+                    self.fg_tiles[coord] = HARD
+                else:
+                    self.fg_tiles[coord] = SOFT
+
+
+    def fgTile(self, coord):
+        """
+        XXX
+        """
+        return self.fg_tiles[coord]
+
+
+    def dropBomb(self, coord, fuse, size):
+        """
+        XXX
+        """
+        defer = Deferred()
+        self._reactor.callLater(fuse, defer.callback, None)
+        return defer
+
+
+
 class Bomb:
     
     def __init__(self, fuse, size):
--- boom/test/test_game.py
+++ boom/test/test_game.py
@@ -1,6 +1,8 @@
 from twisted.trial.unittest import TestCase
+from twisted.internet.task import Clock
 
-from boom.game import Bomb
+from boom.game import Bomb, Board, EMPTY, HARD, SOFT
+
 
 class BombTest(TestCase):
 
@@ -17,3 +19,59 @@ class BombTest(TestCase):
         def check(result):
             self.assertEqual(result, bomb)
         return d.addCallback(check)
+
+
+class BoardTest(TestCase):
+
+    timeout = 2
+
+
+    def test_reactor(self):
+        """
+        You can initialize the Board with any reactor you want
+        (useful for testing).
+        """
+        board = Board(reactor='foo')
+        self.assertEqual(board._reactor, 'foo')
+
+
+    def test_generate(self):
+        """
+        You should be able to generate a standard board of any size,
+        where there's indestructable bricks in a grid like this:
+        """
+        board = Board()
+        board.generate(5, 5)
+        expected = [
+            [SOFT, SOFT, SOFT, SOFT, SOFT],
+            [SOFT, HARD, SOFT, HARD, SOFT],
+            [SOFT, SOFT, SOFT, SOFT, SOFT],
+            [SOFT, HARD, SOFT, HARD, SOFT],
+            [SOFT, SOFT, SOFT, SOFT, SOFT],
+        ]
+        for r,row in enumerate(expected):
+            for c,tile in enumerate(row):
+                actual = board.fgTile((c,r))
+                self.assertEqual(actual, tile, "Expected foreground"
+                                 "tile at %s,%s to be %s, not %s"%(
+                                 c,r,tile,actual))
+
+
+    def test_bomb(self):
+        """
+        You can place a bomb, and get a Deferred back that is called
+        when the bomb goes off.
+        """
+        clock = Clock()
+        board = Board(reactor=clock)
+        board.generate(3,3)
+        d = board.dropBomb((0,0), 10, 1)
+        clock.advance(9)
+        self.assertEqual(d.called, False, "No explosiong yet")
+        clock.advance(1)
+        self.assertEqual(d.called, True, "Now that the time has "
+                         "elapsed, the bomb should have gone off")
+
+
+
+
