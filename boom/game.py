b56d752ba3a461b5ea23526bb60d1aa582f0bf9b
Matt Haggard <haggardii@gmail.com>
2012-06-09 21:28:56 -0600

    Added comments in code to prepare for blog post

--- boom/game.py
+++ boom/game.py
@@ -8,7 +8,6 @@ Board and players and such.
 from twisted.internet import reactor
 from twisted.internet.defer import Deferred
 
-import time
 
 EMPTY = 0
 HARD = 1
@@ -93,9 +92,12 @@ class Board:
         @return: A L{Deferred} that will fire when the bomb 
             explodes.
         """
+        # Set the bomb up to explode later
         defer = Deferred()
         call = self._reactor.callLater(fuse,
                                        self.detonateBomb, coord)
+        
+        # Place the bomb on the board
         self.bombs[coord] = (defer, call, size)
         return defer
 
@@ -107,15 +109,19 @@ class Board:
         @param coord: Tuple coordinate of bomb to detonate.
         """
         defer, call, size = self.bombs[coord]
+        
+        # Remove the bomb from the board
         del self.bombs[coord]
         
+        # Let people who care know that the bomb has exploded
         defer.callback(None)
+        
+        # Premature explosion?
         if call.active():
             call.cancel()
         
-        # we did start the fire
+        # Start the fires up, down, left and right
         self.startFire(coord, self.dft_burn)
-        # up
         directions = [
             (0, -1),
             (0, 1),
@@ -147,9 +153,13 @@ class Board:
         
         @param coord: Coordinate tuple where fire is
         """
-        d, call = self.fires[coord]
-        d.callback(None)
+        d, call = self.fires[coord]        
+        
+        # Remove the fire from the board
         del self.fires[coord]
+        
+        # notify people who care that the fire is out
+        d.callback(None)
 
 
     def startFire(self, coord, burntime):
@@ -174,18 +184,29 @@ class Board:
         
         @return: L{Deferred} which fires when the fire is out.
         """
+        # Is there already a fire on this tile?
         if coord in self.fires:
             defer, call = self.fires[coord]
+            # Stoke the fire
             call.reset(burntime)
         else:
             defer = Deferred()
+            # Put the fire out after a bit.
             call = self._reactor.callLater(burntime, 
                                            self.stopFire, 
                                            coord)
+
+            # Record that there's a fire on the board.
             self.fires[coord] = (defer, call)
+            
+            # Destroy whatever tile is here
             self.fg_tiles[coord] = EMPTY
+            
+            # Is there a bomb to detonate here?
             if coord in self.bombs:
                 self.detonateBomb(coord)
+            
+            # Any pawns that should diaf?
             for pawn in [x for x in self.pawns if x.loc==coord]:
                 pawn.kill()
         return defer
@@ -198,9 +219,12 @@ class Board:
         @param coord: Tuple coordinate to place the pawn
         @param pawn: L{Pawn} to insert.
         """
+        # Let the Board and Pawn know about each other
         pawn.board = self
         pawn.loc = coord
         self.pawns.add(pawn)
+        
+        # Clear a space for the Pawn
         self.fg_tiles[pawn.loc] = EMPTY
         directions = [
             (1,0),
@@ -219,6 +243,8 @@ class Board:
         A pawn has entered a new location
         """
         pawn.loc = new_loc
+        
+        # Did the Pawn unwittingly move into a fire?
         if new_loc in self.fires:
             pawn.kill()
 
@@ -227,6 +253,15 @@ class Pawn:
     """
     I am a player (or NPC) on a L{Board}.  I walk around and put
     bombs down.
+    
+    @param bombs: Number of bombs on hand.
+    @param flame_size: Number of adjacent tiles to blow up
+    @param fuse: Bomb fuse length (in seconds)
+    @param alive: Am I alive, or have I been blown up?
+
+    @param board: The L{Board} I'm playing on
+    @param loc: My current location on the L{Board} as a coordinate
+        tuple.
     """
     
     bombs = 1
@@ -256,12 +291,15 @@ class Pawn:
             raise YoureDead("Dead people can't drop bombs")
         if self.bombs <= 0:
             raise IllegalMove("You're out of bombs")
+
+        # Use a bomb from the Pawn's stash
         self.bombs -= 1
-        d = self.board.dropBomb(self.loc, self.fuse, 
+        d = self.board.dropBomb(self.loc, self.fuse,  
                                 self.flame_size)
+        
+        # Get the bomb back after it explodes
         def bombExploded(result, pawn):
             pawn.bombs += 1
-
         d.addCallback(bombExploded, self)
 
 
@@ -273,7 +311,9 @@ class Pawn:
         speed will be added so that they can't.
         """
         if not self.alive:
-            raise YoureDead("Dead peope can't move")
+            raise YoureDead("Dead people can't move")
+        
+        # Which way?
         delta = {
             'u': (0,-1),
             'd': (0,1),
@@ -281,6 +321,8 @@ class Pawn:
             'r': (1,0),
         }[direction]
         target = (self.loc[0]+delta[0], self.loc[1]+delta[1])
+        
+        # Is it okay to move there?
         try:
             tile = self.board.fg_tiles[target]
         except KeyError:
@@ -289,6 +331,8 @@ class Pawn:
             raise IllegalMove("There's a brick there")
         if target in self.board.bombs:
             raise IllegalMove("There's a bomb there")
+        
+        # Go ahead and move
         self.board.pawnMoved(self, target)
 
 
